fc9fbb7e32c38b10f57c82e988c787a9b539ebf3
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-05-24 11:38:30 +0530

    Add files via upload

---
+++ samantha/config.py
@@ -0,0 +1,27 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "mwklinks")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/70c4295b52a5559afdc15.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "krithiXsamantha")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "redbullfed")
+PROJECT_NAME = getenv("PROJECT_NAME", "Samantha V2")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/shamilhabeebnelli/samanthaXcalls")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
